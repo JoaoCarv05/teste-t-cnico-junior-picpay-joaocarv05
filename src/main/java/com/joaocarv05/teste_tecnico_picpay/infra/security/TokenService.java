b39edeb9c3d74517665814c943704001a8db0efd
João Victor Castro de Carvalho <joaovic.castroo@gmail.com>
2025-01-02 10:54:06 -0300

    feature: agora tokenService define uma interface para acessar metodos de criação e verificação de tokens

--- src/main/java/com/joaocarv05/teste_tecnico_picpay/infra/security/TokenService.java
+++ src/main/java/com/joaocarv05/teste_tecnico_picpay/infra/security/TokenService.java
@@ -1,68 +1,8 @@
 package com.joaocarv05.teste_tecnico_picpay.infra.security;
 
 import com.joaocarv05.teste_tecnico_picpay.domain.user.User;
-import com.nimbusds.jose.*;
-import com.nimbusds.jose.crypto.MACSigner;
-import com.nimbusds.jose.crypto.MACVerifier;
-import com.nimbusds.jwt.JWTClaimsSet;
-import com.nimbusds.jwt.SignedJWT;
-import org.springframework.beans.factory.annotation.Value;
-import org.springframework.stereotype.Service;
-import java.text.ParseException;
-import java.time.LocalDateTime;
-import java.time.ZoneOffset;
-import java.util.Date;
 
-@Service
-public class TokenService {
-
-    @Value("${jwttoken.jwtsecret.value}")
-    String JWTsecret;
-    @Value("${jwttoken.expiration.value}")
-    int JWTexpirationTimeInMinutes;
-    /**
-     * retorna um JWT com o login do usúario assinado.
-     *
-     * @param user
-     * @return JWT
-     * @throws JOSEException
-     */
-    public String sign(User user) throws JOSEException {
-        JWSSigner jwsSigner = new MACSigner(JWTsecret);
-        JWTClaimsSet claimsSet = new JWTClaimsSet.Builder()
-                .issuer("API")
-                .subject("my-api")
-                .expirationTime(new Date(new Date().getTime() + 60 * 60 * 1000))
-                .claim("login", user.getEmail())
-                .build();
-        SignedJWT signedJWT = new SignedJWT(new JWSHeader(JWSAlgorithm.HS256
-        ), claimsSet);
-        signedJWT.sign(jwsSigner);
-        return signedJWT.serialize();
-    }
-
-    /**
-     * verifica a autenticidade do token.
-     *
-     * @param token
-     * @return retorna o login do token caso sua autenticidade seja comprovada
-     * @throws JOSEException
-     * @throws ParseException
-     */
-    public String verifyToken(String token) throws JOSEException, ParseException {
-        SignedJWT signedJWT = SignedJWT.parse(token);
-        JWTClaimsSet claims = signedJWT.getJWTClaimsSet();
-        JWSVerifier verifier = new MACVerifier(JWTsecret);
-        if (!signedJWT.verify(verifier) || new Date().after(claims.getExpirationTime())) {
-            throw new JOSEException("token inválido e/ou expirado");
-        } else {
-            return claims.getStringClaim("login");
-        }
-    }
-
-    private Date expirationTokenTime() {
-        LocalDateTime now = LocalDateTime.now();
-        LocalDateTime newTime = now.plusMinutes(JWTexpirationTimeInMinutes);
-        return Date.from(newTime.toInstant(ZoneOffset.of("-03:00")));
-    }
+public interface TokenService {
+    public String createSignedJWT(User user);
+    public String verifyToken (String token);
 }
