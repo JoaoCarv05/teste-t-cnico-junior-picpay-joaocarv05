2fcf8649b1aa364e58c70007062a983c6c033f1d
João Victor Castro de Carvalho <joaovic.castroo@gmail.com>
2025-01-02 11:01:12 -0300

    feature adiciona uma implmentação personalizada de AuthenticationEntryPoint para delegar as exeções do spring security a um globalHandler do spring MVC

---
+++ src/main/java/com/joaocarv05/teste_tecnico_picpay/infra/security/CustomAuthenticationEntryPoint.java
@@ -0,0 +1,25 @@
+package com.joaocarv05.teste_tecnico_picpay.infra.security;
+
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import org.springframework.security.core.AuthenticationException;
+import org.springframework.security.web.AuthenticationEntryPoint;
+import org.springframework.stereotype.Component;
+import org.springframework.web.servlet.HandlerExceptionResolver;
+
+@Component
+public class CustomAuthenticationEntryPoint implements AuthenticationEntryPoint {
+
+    private final HandlerExceptionResolver handlerExceptionResolver;
+
+    public CustomAuthenticationEntryPoint(HandlerExceptionResolver handlerExceptionResolver) {
+        this.handlerExceptionResolver = handlerExceptionResolver;
+    }
+
+    @Override
+    public void commence(HttpServletRequest request,
+                         HttpServletResponse response,
+                         AuthenticationException authException) {
+        handlerExceptionResolver.resolveException(request, response, null, authException);
+    }
+}
